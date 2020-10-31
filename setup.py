3e596451e0d96d13f6d53782f778d8c2c534d19e
Alexander Gugel <alexander.gugel@gmail.com>
2020-10-31 18:06:28 +0000

    Add py.typed marker

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alexanderGugel/nate",
+    package_data={"nate": ["py.typed"]},
     packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         "Programming Language :: Python :: 3",
