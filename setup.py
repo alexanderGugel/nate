4504445c704f5b35faf3d623eb306808a9d88087
Alexander Gugel <alexander.gugel@gmail.com>
2020-11-01 17:05:54 +0000

    0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nate",
-    version="0.0.5",
+    version="0.0.6",
     author="Alexander Gugel",
     author_email="alexander.gugel@gmail.com",
     description="Make generating HTML fun.",
